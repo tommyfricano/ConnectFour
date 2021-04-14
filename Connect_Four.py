df8dffc57f25f7ba50b89120cd12b59976960bd5
tommy fricano <tjfricano@gmail.com>
2021-04-14 17:21:45 -0400

    progress with new logic for checking win, with few other methods within Game class

--- Connect_Four.py
+++ Connect_Four.py
@@ -1,20 +1,60 @@
 import random
+import numpy as np
 
 class Game:
-    def __init__(self, player_count, game_count):
-        self.player_count = player_count
-        self.game_count = game_count
+    def __init__(self ):
+        self.row_count = 6
+        self.column_count = 7
+        #self.game_count = game_count
+        self.board = np.zeros((self.row_count,self.column_count))
     
     def create_game(self):
-        print("\n|  |  |  |  |  |  |  |\n|  |  |  |  |  |  |  |\n|  |  |  |  |  |  |  |\n|  |  |  |  |  |  |  |\n|  |  |  |  |  |  |  |\n|  |  |  |  |  |  |  |")
+        return np.flip(self.board, 0)
 
+    def open_spot(self, column):
+        return self.board[self.row_count-1][column] == 0
+
+    def play(self, row, col, move):
+        self.board[row][col] = move
+
+    def check_win(self, move):
+                                                                    #checks vertical
+        for c in range(self.column_count):
+            for r in range(self.row_count):
+                if self.board[r][c] == move and self.board[r+1][c] and self.board[r+2][c] == move and self.board[r+3][c] == move:
+                    return True
+                                                                    #checks horizontal
+        for c in range(self.column_count):
+            for r in range(self.row_count):
+                if self.board[r][c] == move and self.board[r][c+1] and self.board[r][c+2] == move and self.board[r][c+3] == move:
+                    return True   
+                                                                    #checks left to right diaganol 
+        for c in range(self.column_count):
+            for r in range(self.row_count):
+                if self.board[r][c] == move and self.board[r+1][c+1] and self.board[r+2][c+2] == move and self.board[r+3][c+3] == move:
+                    return True
+        
+        for c in range(self.column_count):
+            for r in range(self.row_count):
+                if self.board[r][c] == move and self.board[r-1][c+1] and self.board[r-2][c+2] == move and self.board[r-3][c+3] == move:
+                    return True
 
 #Setup player(s)
 print("Connect Four!" + "\n(Press enter when ready to play!)")
 input()
-num_players = input("How many players(type 1 or 2)\n") 
-num_games = input("How many games do you want to play: \n1)One game \n2)Best of three \n3)Best of five \n")
+#num_games = input("How many games do you want to play: \n1)One game \n2)Best of three \n3)Best of five \n")
+finished = False
+turn = 0 
 
-new_game = Game(num_players, num_games)
-new_game.create_game()
+connect = Game(num_games)
+connect.create_game()
 
+while not finished:
+    if turn % 2 == 0:
+        current_player = "player1"
+        piece = "x"
+    else:
+        current_player = "player2"
+        piece = "o"
+    if connect.open_spot(int(input("Where do you want to drop? (1-6)"))) == True:
+        connect.play()
